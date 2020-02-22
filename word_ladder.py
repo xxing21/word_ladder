7a647b73afed1164874589656994251445e75f72
xxing21 <xxing21@cmc.edu>
2020-02-21 21:49:58 -0800

    code

--- word_ladder.py
+++ word_ladder.py
@@ -28,6 +28,28 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     Whenever it is impossible to generate a word ladder between the two words,
     the function returns `None`.
     '''
+    from collections import deque
+    from copy import deepcopy
+
+    f = open('words5.dict')
+    dictionary = f.read().split("\n")
+
+    s = []
+    s.append(start_word)
+    q = deque()
+    q.append(s)
+
+    while len(q) > 0:
+        top = q.pop()[0]
+        for word in dictionary:
+            if _adjacent(top, word) is True:
+                if word == end_word:
+                    s.append(word)
+                    return s
+                copy = deepcopy(s)
+                copy.append(word)
+                q.append(copy)
+                del word
 
 
 def verify_word_ladder(ladder):
@@ -35,6 +57,14 @@ def verify_word_ladder(ladder):
     Returns True if each entry of the input list is adjacent to its neighbors;
     otherwise returns False.
     '''
+
+    if len(ladder) == 0:
+        return False
+    else:
+        for i in range(len(ladder)-1):
+            if _adjacent(ladder[i], ladder[i+1]) is False:
+                return False
+        return True
 
 
 def _adjacent(word1, word2):
@@ -47,3 +77,15 @@ def _adjacent(word1, word2):
     >>> _adjacent('stone','money')
     False
     '''
+
+    diff = 0
+    if len(word1) != len(word2):
+        return False
+    else:
+        for i in range(len(word1)):
+            if word1[i] != word2[i]:
+                diff = diff + 1
+        if diff == 1:
+            return True
+        else:
+            return False
